e3a46a2b77a28e23e445bda91f8777e2c87a8dba
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-20 15:48:11 +0100

    ESERCIZIO COMPLETATO +Bonus

--- js/script.js
+++ js/script.js
@@ -34,15 +34,27 @@ createApp({
             this.todos.splice(this.todos.indexOf(todo), 1);
         },
 
+        //aggiungo il todo alla lista con l'input
         addTodo() {
             //aggiungo un nuovo oggetto todo all'array
+
+            //controllo per impedire l'inserimento di un campo vuoto
+            if (this.newTodo.trim() === '') {
+                return;
+            }
+
             this.todos.push({
               text: this.newTodo,
               done: false,
             });
-            
+
             //svuoto il campo di input
             this.newTodo = '';
-          },
+        },
+
+        //function per alternare il "done" dei todo da true a false
+        signTodo(todo) {
+            todo.done = !todo.done;
+        }
     },
 }).mount('#app');
