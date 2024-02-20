65043a52a5315b55595790125a20e3692eccb8c7
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-20 13:23:13 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const {createApp} = Vue
 createApp({
     data() {
         return {
-            
+            todos: [
+                {
+                  text: 'Comprare il latte',
+                  done: false,
+                },
+                {
+                  text: 'Fare il bucato',
+                  done: true,
+                },
+                {
+                  text: 'Andare dal dottore',
+                  done: false,
+                },
+                {
+                    text: 'Finire il lavoro su VS',
+                    done: true,
+                },
+            ],
         }
     },
     methods: {
